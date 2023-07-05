a1dc0b9eeddb3d7c70f4be13339ac38d8819bcb7
skakwy <nils_0805@icloud.com>
2023-07-05 19:54:15 +0200

    website

--- server/debugRunner.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <cstdlib> // For system()
-
-int main() {
-    // Run a command in the command line
-    system("g++ -o program main.cpp -std=c++2a -ljsoncpp -lsqlite3");
-    system("./program");
-    return 0;
-}
